a0eb20ffa8038e26df86fd53eaa4ff1faf1fbaa6
Cade <caden_engle5@yahoo.com>
2022-04-24 20:49:07 -0500

    sanity backend setup/troubles

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -3,31 +3,18 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants'
 import './About.scss';
-
-const abouts = [
-	{
-		title: 'Web Development',
-		description: 'I am a good web developer.',
-		imgUrl: images.about01,
-	},
-	{
-		title: 'Web Design',
-		description: 'I am a good web developer.',
-		imgUrl: images.about02,
-	},
-	{
-		title: 'UI/UX',
-		description: 'I am a good web developer.',
-		imgUrl: images.about03,
-	},
-	{
-		title: 'Web Animations',
-		description: 'I am a good web developer.',
-		imgUrl: images.about04,
-	},
-];
+import { urlFor, client } from '../../client';
 
 const About = () => { 
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+  }, []);
+
   return (
     <>
       <h2 className="head-text">I know that <span>Good Dev</span><br />means <span>Good Business</span></h2>
