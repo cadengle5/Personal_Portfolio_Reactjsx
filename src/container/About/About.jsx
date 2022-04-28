13769060ca2b5f7c5dbf813835c429e64805ef39
Cade <caden_engle5@yahoo.com>
2022-04-28 16:33:17 -0500

    Fixed ID & Token issue

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -28,9 +28,9 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
-            <h2 className="bold-text" style={{marginTop: 20 }}>{about.title}</h2>
-            <p className="p-text" style={{marginTop: 10 }}>{about.description}</p>
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
         ))}
       </div>
